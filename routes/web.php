40229868778f3d3318b056bee3996e566ccecca6
Reza alamsyah <reza.alamsyah2013@gmail.com>
2024-09-04 14:17:54 +0700

    new file:   app/Http/Controllers/ArticleController.php 	new file:   app/Http/Controllers/HomeController.php 	new file:   app/Http/Controllers/PageController.php 	new file:   app/Http/Controllers/PhotoController.php 	new file:   app/Http/Controllers/WelcomeController.php 	new file:   app/Http/Controllers/aboutController.php 	new file:   resources/views/blog/hello.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\aboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class,'home']);
+Route::get('/hello',[WelcomeController::class,'hello']);
+Route::get('/world', function () {    return 'World'; 
+}); 
+Route::get('/about', [aboutController::class,'about']);
+
+Route::get( '/user/{name}', function ($name) { return 'Nama saya '.$name; });
+Route::get('/posts/{post}/comments/{comment}' , function ($postId, $commentId){
+    return 'Pos ke - ' .$postId."Komentar ke-:".$commentId ;
 });
+Route::get('/articles/{id}' ,[ArticleController::class,'articles']);
+
+Route::get( '/user/{name?}', function ($name = 'John') { return 'Nama saya '.$name; });
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only(['index','show']);
+Route::resource('photos', PhotoController::class)->except(['create','store','update',]);
+
+Route::get('/greeting', [WelcomeController::class,'greeting']);
